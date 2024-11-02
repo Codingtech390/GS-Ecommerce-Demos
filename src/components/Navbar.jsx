923ecdcb760122b963a82814aedd9b7d4616fc73
codingtech <codingtech390@gmail.com>
2024-11-02 10:18:46 +0530

    Fixed minor issues in navbar. both in mobile and desktop view.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,9 @@ const Navbar = () => {
 
   return (
     <div className="flex items-center justify-between py-5 font-medium">
-      <img src={assets.logo2} alt="logo1" className="w-16" />
+      <Link to={"/"}>
+        <img src={assets.logo2} alt="logo1" className="w-16" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1">
