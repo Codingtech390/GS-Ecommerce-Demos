17de3dcd08840af1717e8fc2fb734fcbcc4634bd
codingtech <codingtech390@gmail.com>
2024-11-07 10:13:54 +0530

    UI Design Completed. Copyright and footer sections have been added.

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Orders from "./pages/Orders";
 import Collection from "./pages/Collection";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 const App = () => {
@@ -28,6 +29,7 @@ const App = () => {
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/product/:productId" element={<Product />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
