ea0ee69b082d0bc97bb57b0190208eccac72b4b4
codingtech <codingtech390@gmail.com>
2024-11-07 09:49:09 +0530

    NewsletterBox and OurPolicy Componets added.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
-import Hero from '../components/Hero'
-import LatestCollection from '../components/LatestCollection'
+import React from "react";
+import Hero from "../components/Hero";
+import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Home = () => {
   return (
     <div>
-      
-      {/* Hero Section */}
       <Hero />
 
-      {/* LatestCollection */}
       <LatestCollection />
-    
-    </div>
-  )
-}
 
-export default Home
+      <BestSeller />
+
+      <OurPolicy />
+
+      <NewsletterBox />
+    </div>
+  );
+};
+
+export default Home;
