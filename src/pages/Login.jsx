b3b034e19eba104ac076a8f9c6dc1355b25e56fc
codingtech <codingtech390@gmail.com>
2024-11-23 14:17:01 +0530

    Completed UI for contact us page. With this UI part has now been completed.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,9 +10,9 @@ const Login = () => {
   return (
     <form
       onSubmit={onSubmitHandler}
-      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-700"
+      className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-700 bg-slate-50 p-4 rounded-md"
     >
-      <div className="inline-flex items-center gap-2 mb-2 mt-10">
+      <div className="inline-flex items-center gap-2 mb-2 mt-2">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800" />
       </div>
