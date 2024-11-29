a4ca8d2fc7dbd6d5c21b2f0351a0a73665c6dfc2
codingtech <codingtech390@gmail.com>
2024-11-29 23:25:30 +0530

    Created Routes and controllers for register, login and admin. Register user api is working till here.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 // App configuration
 const app = express();
@@ -18,6 +19,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Api working");
 });
+
+app.use('/api/user', userRouter);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
