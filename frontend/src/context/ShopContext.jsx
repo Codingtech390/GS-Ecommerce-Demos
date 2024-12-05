a7203a30bc8ce6c2e2d0091c948cbdd40d223f9f
codingtech <codingtech390@gmail.com>
2024-12-05 21:38:00 +0530

    fetchcartData is now complete.

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -81,20 +81,40 @@ const ShopContextProvider = (props) => {
     cartData[itemId][size] = quantity;
 
     setCartItems(cartData);
+
+    if (token) {
+      try {
+        await axios.post(
+          backendUrl + "/api/cart/update",
+          { itemId, size, quantity },
+          { headers: { token } }
+        );
+      } catch (error) {
+        console.error(error);
+        toast.error(error.message);
+      }
+    }
   };
 
   const getCartAmount = () => {
     let totalAmount = 0;
     for (const items in cartItems) {
       let itemInfo = products.find((product) => product._id === items);
+      
+      if (!itemInfo) {
+        console.log(`Product with ID ${items} not found.`);
+        continue; // Skip to the next item if the product is not found
+      }
+
 
       for (const item in cartItems[items]) {
         try {
           if (cartItems[items][item] > 0) {
             totalAmount += itemInfo.price * cartItems[items][item];
           }
-        } catch (err) {
-          console.error(err);
+        } catch (error) {
+          console.log(error);
+          toast.error(error.message);
         }
       }
     }
@@ -117,6 +137,23 @@ const ShopContextProvider = (props) => {
     }
   };
 
+  const getUserCartInformation = async (token) => {
+    try {
+      const response = await axios.post(
+        backendUrl + "/api/cart/get",
+        {},
+        { headers: { token } }
+      )
+
+      if (response.data.success) {
+        setCartItems(response.data.cartData);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
   useEffect(() => {
     getProductsData();
     // eslint-disable-next-line
@@ -125,6 +162,7 @@ const ShopContextProvider = (props) => {
   useEffect(() => {
     if (!token && localStorage.getItem("token")) {
       setToken(localStorage.getItem("token"));
+      getUserCartInformation(localStorage.getItem("token"));
     }
   }, []);
 
