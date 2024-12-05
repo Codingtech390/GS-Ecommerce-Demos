6db27af4eae40a157e3c12c6fe15669023b178a9
codingtech <codingtech390@gmail.com>
2024-12-05 10:44:15 +0530

    Backend is now connected to the frontend code.

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,16 +1,25 @@
 import { createContext, useEffect, useState } from "react";
-import { products } from "../assets/assets";
+// import { products } from "../assets/assets";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
+import { backendURL } from "../../../admin/src/App";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
+
+  // backend url
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+
+  const [products, setProducts] = useState([]);
 
   const navigate = useNavigate();
 
@@ -78,6 +87,27 @@ const ShopContextProvider = (props) => {
     return totalAmount;
   };
 
+  // Get products data from backend
+
+  const getProductsData = async () => {
+    try {
+      const response = await axios.get(backendURL + "/api/product/list");
+      if (response.data.success) {
+        setProducts(response.data.products);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getProductsData();
+    // eslint-disable-next-line
+  }, []);
+
   const value = {
     products,
     currency,
@@ -92,6 +122,7 @@ const ShopContextProvider = (props) => {
     updateQuantity,
     getCartAmount,
     navigate,
+    backendUrl,
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
